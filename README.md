8b91577e78c9ce65147c6fb07dec66ee5c27d5bb
d2lao <d2lao@student.cs.uwaterloo.ca>
2020-12-08 11:38:38 -0500

    backup

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Straights
+
+Board: To store the current board of the game
+Card: Stores the rank and suit of a card
+Computer: The methods for a computer player
+Controller: Interprets and processes commands given
+Deck: The deck with all the cards
+Human: The methods for a human player
+Observer & Subject: Part of design pattern, add and takes off observers
+State: The current state of the game ()
+View: Does the output of the game
+Player: The methods for any player, will call the corresponding methods for computer/human
--- player.h
+++ player.h
@@ -2,11 +2,39 @@
 #define PLAYER_H
 #include "card.h"
 #include <vector>
+#include <memory>
+
+enum class PlayerType { Human, Computer };
 
 class Player {
-    
-    public:
-
+protected:
+    // The score of the player
+    int score = 0;
+    // What cards the player has discarded
+    std::vector<std::shared_ptr<Card>> discardpile;
+    // What cards are in the players hand
+    std::vector<std::shared_ptr<Card>> hand;
+    // What cards can the player play
+    std::vector<std::shared_ptr<Card>> validplays;
+    // What type of player: human/computer
+    PlayerType type;
+public:
+    // Constructs a player with the given type
+    Player(PlayerType type);
+    // Adds card to hand
+    void addCard(Card c);
+    // Switches the type of player: from human -> computer or vice versa
+    void switchType();
+    // Whether or not the player will start the game (has seven of spades)
+    bool willStart();
+    // Computes the new score of the player (after a round has completed)
+    int roundScore();
+    // Return the total score
+    int totalScore();
+    // Recalculates the valid plays of the player
+    void recalculateValid();
+    // Returns the list of valid plays for this player
+    const std::vector<shared_ptr<Card>> getValidPlays();
 }
 
 #endif
